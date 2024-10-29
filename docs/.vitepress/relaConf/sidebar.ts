5781f6a92cfd867ce0c37d8e5c651997859f9e6d
胡雨屹 <huyuyi@cestc.cn>
2024-10-29 11:37:43 +0800

    uni-app

--- docs/.vitepress/relaConf/sidebar.ts
+++ docs/.vitepress/relaConf/sidebar.ts
@@ -1,13 +1,19 @@
-import { DefaultTheme } from "vitepress"
+import { DefaultTheme } from 'vitepress';
 export const sidebar: DefaultTheme.Sidebar = {
-    '/column/CSS/': [
-        {
-            text: 'Flex 布局',
-            link: '/column/CSS/flex'
-        },
-        {
-            text: 'Grid 布局',
-            link: '/column/CSS/grid'
-        }
-    ]
-}
+  '/column/CSS/': [
+    {
+      text: 'Flex 布局',
+      link: '/column/CSS/flex'
+    },
+    {
+      text: 'Grid 布局',
+      link: '/column/CSS/grid'
+    }
+  ],
+  '/column/Git/': [
+    {
+      text: '创建版本库',
+      link: '/column/Git/01'
+    }
+  ]
+};
---
+++ docs/column/uni-app/index.md
@@ -0,0 +1,19 @@
+# Vite + Vue3 + TypeScript + VSCode 项目搭建
+
+```sh
+# 从 gitee 上下载项目模板：
+git clone -b uni-app-project https://gitee.com/dcloud/uni-reset-vue.git
+```
+
+## vscode 开发配置
+
+插件：
+
+Vue3 插件：
+- Vue - Official ：Vue3 语法提示插件
+- TypeScript Vue Plugin (Volar) ：Vue3 + TS 插件
+
+uni-app 插件：
+- uni-create-view ：快速创建 uni-app 页面
+- uni-helper ：代码提示
+- uniapp 小程序扩展 ：鼠标悬停查文档
