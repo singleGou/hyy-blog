9f5d5145c3486944350dfdede6e14489248385cd
singleGou <huyuyiye@qq.com>
2024-10-30 18:06:36 +0800

    feat: ts

--- docs/.vitepress/relaConf/sidebar.ts
+++ docs/.vitepress/relaConf/sidebar.ts
@@ -12,8 +12,28 @@ export const sidebar: DefaultTheme.Sidebar = {
   ],
   '/column/TypeScript/': [
     {
-      text: 'Flex 布局',
+      text: '类型系统',
       link: '/column/TypeScript/01-类型系统.md',
+    },
+    {
+      text: '数组',
+      link: '/column/TypeScript/02-数组.md',
+    },
+    {
+      text: '元组',
+      link: '/column/TypeScript/03-元组.md',
+    },
+    {
+      text: 'symbol 类型',
+      link: '/column/TypeScript/04-symbol类型.md',
+    },
+    {
+      text: '函数',
+      link: '/column/TypeScript/05-函数.md',
+    },
+    {
+      text: '对象',
+      link: '/column/TypeScript/06-对象.md',
     },
   ],
 }
--- docs/column/TypeScript/04-symbol类型.md
+++ docs/column/TypeScript/04-symbol类型.md
@@ -1,4 +1,4 @@
-# symbol 类型
+## symbol 类型
 
 类似于字符串，但是每一个 Symbol 值都是独一无二的，与其他任何值都不相等，Symbol 值通过 Symbol() 函数生成：
 ```ts
@@ -8,7 +8,7 @@ let y: symbol = Symbol()
 x === y // false
 ```
 
-# unique symbol 类型
+## unique symbol 类型
 
 unique symbol 类型的一个作用，就是用作属性名，这可以保证不会跟其他属性名冲突。如果要把某一个特定的 Symbol 值当作属性名，那么它的类型只能是 unique symbol，不能是 symbol：
 ```ts
